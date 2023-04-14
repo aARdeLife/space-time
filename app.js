733ff22cc61924f4ce4b1a6ab29d36d4dbeedbea
Super Vision <107816707+aARdeLife@users.noreply.github.com>
2023-04-14 22:08:55 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,55 +1,46 @@
-const video = document.getElementById('video');
-const canvas = document.getElementById('canvas');
-const context = canvas.getContext('2d');
+// script.js
+const videoElement = document.getElementById('video');
+const canvasElement = document.getElementById('canvas');
+const canvasCtx = canvasElement.getContext('2d');
 
 async function setupCamera() {
-    const constraints = {
-        video: {
-            facingMode: { ideal: "environment" },
-            width: { ideal: 640 },
-            height: { ideal: 480 }
-        }
+  const stream = await navigator.mediaDevices.getUserMedia({
+    audio: false,
+    video: { facingMode: 'user' },
+  });
+
+  videoElement.srcObject = stream;
+  return new Promise((resolve) => {
+    videoElement.onloadedmetadata = () => {
+      resolve(videoElement);
     };
-
-    try {
-        video.srcObject = await navigator.mediaDevices.getUserMedia(constraints);
-        return new Promise(resolve => video.onloadedmetadata = resolve);
-    } catch (error) {
-        console.error("Error accessing camera:", error);
-    }
+  });
 }
 
-async function detectObjects(model) {
-    const predictions = await model.detect(video);
-    drawBoundingBoxes(predictions);
-    requestAnimationFrame(() => detectObjects(model));
+async function setupIrisTracking() {
+  const iris = new Iris({ locateFile: (file) => `https://cdn.jsdelivr.net/npm/@mediapipe/iris/${file}` });
+  iris.setOptions({ maxNumIrisLandmarks: 1 });
+  iris.onResults(onResults);
+
+  const camera = await setupCamera();
+  camera.width = videoElement.videoWidth;
+  camera.height = videoElement.videoHeight;
+  canvasElement.width = videoElement.videoWidth;
+  canvasElement.height = videoElement.videoHeight;
+
+  function renderFrame() {
+    canvasCtx.drawImage(videoElement, 0, 0, videoElement.videoWidth, videoElement.videoHeight);
+    iris.send({ image: videoElement });
+    requestAnimationFrame(renderFrame);
+  }
+  renderFrame();
 }
 
-function drawBoundingBoxes(predictions) {
-    context.clearRect(0, 0, canvas.width, canvas.height);
-    context.drawImage(video, 0, 0, canvas.width, canvas.height);
-
-    predictions.forEach(prediction => {
-        const [x, y, width, height] = prediction.bbox;
-
-        context.strokeStyle = 'red';
-        context.lineWidth = 3;
-        context.strokeRect(x, y, width, height);
-
-        context.font = '18px Arial';
-        context.fillStyle = 'red';
-        context.fillText(prediction.class, x, y - 5);
-    });
+function onResults(results) {
+  canvasCtx.clearRect(0, 0, canvasElement.width, canvasElement.height);
+  canvasCtx.drawImage(results.segmentationMask, 0, 0, canvasElement.width, canvasElement.height);
+  drawLandmarks(canvasCtx, results.multiIrisLandmarks, { lineWidth: 1, color: 'blue' });
+  // Use the pupil position as a pointer
 }
 
-async function main() {
-    await setupCamera();
-    video.play();
-    canvas.width = video.videoWidth;
-    canvas.height = video.videoHeight;
-
-    const model = await tfModels.cocoSsd.load();
-    detectObjects(model);
-}
-
-main();
+setupIrisTracking();
